fcb25fd6d9b70dd637678c5c465734f5a7bd035a
TuanAnh <vtabk2@gmail.com>
2017-09-22 18:54:10 +0700

    ref fix

--- app/src/main/java/com/tuananh/inputfilter/MainActivity.java
+++ app/src/main/java/com/tuananh/inputfilter/MainActivity.java
@@ -1,12 +1,15 @@
 package com.tuananh.inputfilter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditTextCustom editNormal = (EditTextCustom) findViewById(R.id.edit_normal);
+        editNormal.setFilters(new InputFilter[]{CommonUtils.getInputFilter()});
     }
 }
